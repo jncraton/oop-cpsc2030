466723db912314bd21db6a5af76394e4f88baac0
Jon Craton <jncraton@gmail.com>
2025-04-15 15:00:49 -0400

    Add constructor.cc

---
+++ examples/cpp/constructor.cc
@@ -0,0 +1,19 @@
+import std;
+
+class Person {
+private:
+    std::string name;
+    int age;
+
+public:
+    Person(std::string name, int age) : name(name), age(age) {}
+
+    void display() const {
+        std::println("Name: {}, Age: {}", name, age);
+    }
+};
+
+int main() {
+    Person p("Alice", 30);
+    p.display();
+}
